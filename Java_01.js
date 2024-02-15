b6f5fdbad331162a73f44cd7cc63be247ebd78bd
LomanowaZvigintseva <145358789+LomanowaZvigintseva@users.noreply.github.com>
2024-02-15 11:40:14 +0300

    Add files via upload

---
+++ Java_01.js
@@ -0,0 +1,14 @@
+'Привет, Java!'
+"В понедельник 2 заболевших орз"
+"во вторник 3 заболевших орз и один ветрянкой"
+"среда 4 заболевших ветрянкой" 
+"среда 2 заболевших орз"
+var num_orz_mon= 2
+var num_orz_tue = 3
+var num_orz_wedn = 2
+var num_vetryanka_mon = 0
+var num_vetryanka_tue = 1
+var num_vetryanka_wedn = 4
+var num_orz_week=num_orz_mon+num_orz_tue+num_orz_wedn
+var num_vetryanka_week=num_vetryanka_mon+num_vetryanka_tue+num_vetryanka_wedn
+num_vetryanka_mon+num_vetryanka_tue+num_vetryanka_wedn + " vetryanka"
